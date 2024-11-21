ccd4e7036358a6d0fb1dd63a96297712f65e227f
shahdramadan655 <shahdramadan655@gmail.com>
2024-11-21 21:47:42 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using SchoolProject.Context;
+using SchoolProject.Repository;
+
+namespace SchoolProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
+        public IConfiguration Configuration { get; }
+
+        // this method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<ApplicationContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")
+                )
+
+           );
+            
+            services.AddDefaultIdentity<ApplicationUsers>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+
+            services.AddTransient<IStudentRepository, StudentRepository>();
+            services.AddTransient<ITeacherRepository, TeacherRepository>();
+            services.AddTransient<IRoomRepository, RoomRepository>();
+            services.AddTransient<ICourseRepository, CourseRepository>();
+        }
+
+        // this method gets called by the runtime. Use this method configure the HTTP request pipline.
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
+            }
+            );
+        }
+    }
+}
+
+
+
